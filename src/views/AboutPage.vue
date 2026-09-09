dd9e017c4d1296906edfb61ce73221eb39e3edb7
mallorysmith64 <mallorysmith64@gmail.com>
2026-09-09 11:34:28 -0400

    updated About screen with inclusive lang of space launches

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,26 +1,27 @@
 <template>
   <section class="section container">
     <h1 class="section-title">Tracking Missions</h1>
-    <p class="section-lede">This is an informational site about SpaceX's missions</p>
+    <p class="section-lede">This is an informational site about different space missions</p>
 
     <div class="about__grid">
       <article class="about__card">
         <h3>Mission Types</h3>
         <p>
-          SpaceX conducts a variety of missions, including satellite launches, cargo resupply
-          missions, and crewed spaceflights.
+          There are several types of space missions, including cargo resupply missions, satellite
+          deployments, and crewed spaceflights.
         </p>
       </article>
 
       <article class="about__card">
         <h3>History</h3>
-        <p>The purpose of this site is to give a historical lens of SpaceX's launches.</p>
+        <p>The purpose of this site is to show different space launches.</p>
       </article>
 
       <article class="about__card">
-        <h3>Not affiliated with SpaceX</h3>
+        <h3>No affiliation</h3>
         <p>
-          This site is not affiliated with SpaceX and is intended for informational purposes only.
+          This site is not affiliated with any space company and is intended for informational
+          purposes only.
         </p>
       </article>
     </div>
